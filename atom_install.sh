c0323167081982e5ec7d4bc3afeedd0f14fdc339
Bryan Hernandez <pdnbryan1998@gmail.com>
2018-09-10 11:35:30 -0700

    Update atom_install.sh

--- atom_install.sh
+++ atom_install.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+sudo apt update
+sudo apt install curl apt-transport-https
 curl -sL https://packagecloud.io/AtomEditor/atom/gpgkey | sudo apt-key add -
 sudo sh -c 'echo "deb [arch=amd64] https://packagecloud.io/AtomEditor/atom/any/ any main" > /etc/apt/sources.list.d/atom.list'
 sudo apt-get update
